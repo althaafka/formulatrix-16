9da11fc2539eb490ebcf5966a6455a421bea4f85
althaafka <althaafka004@gmail.com>
2025-11-05 11:49:42 +0700

    feat(day 01): logic exercise

---
+++ day 01/LogicExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int x = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i <= x; i++)
+{
+    if (i % 15 == 0)
+    {
+        Console.Write("foobar");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.Write("foo");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.Write("bar");
+    }
+    else
+    {
+        Console.Write(i);
+    }
+    if (i != x) Console.Write(", ");
+}
